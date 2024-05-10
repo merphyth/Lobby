888df55d8bac79cd62fe19f71e9acdedf5ea0d58
merphyth <senyachumak@gmail.com>
2024-05-10 14:56:57 +0200

    asd

---
+++ ASP_NET_Practice_Module_01_part_01/ASP_NET_Practice_Module_01_part_01/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ASP_NET_Practice_Module_01_part_01.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
---
+++ module3HW1/WebApplication1/Pages/1dot1.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class _1dot1Model : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
